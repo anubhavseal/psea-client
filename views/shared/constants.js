d4a3b960564191c5ff120cf1fafb5a9ea6dba3ee
MOTIFWORKS\Aditya.Chougule <aditya.chougule@motifworks.com>
2017-10-26 14:01:36 +0530

    PSEA org App ID included in Constant

--- views/shared/constants.js
+++ views/shared/constants.js
@@ -15,7 +15,7 @@ angular.module('shared').constant('$constants', {
 	'CACHE_ENABLED': true,
 	'SupportedAuthentications': [
 		{'type': 'DB', 'caption': 'Database', 'icon': null},
-		{'type': 'O365', 'caption': 'O365', 'icon': '/images/office-365-cloud-png-12.png', 'config': {instance: 'https://login.microsoftonline.com/', tenant: 'motifworks.com', clientId: 'b28c22a2-e7c9-498a-99be-16c726703418', extraQueryParameter: 'nux=1'}}
+		{'type': 'O365', 'caption': 'O365', 'icon': '/images/office-365-cloud-png-12.png', 'config': {instance: 'https://login.microsoftonline.com/', tenant: 'psea.org', clientId: '6f00369c-a12e-427a-b918-1443d5684d4c', extraQueryParameter: 'nux=1'}}
 	],
 	'Functions': {
 		'ManageTenantSpecificProfile': 6,
